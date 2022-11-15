7f95c7b5208efd3ec65a923f91a98cfb40e9d6b8
Thuan Tran <thuantd2001hb@gmail.com>
2022-11-15 20:24:43 +0700

    a

---
+++ MyWebApiApp/MyWebApiApp/Models/PaginatedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyWebApiApp.Models
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPage = (int)Math.Ceiling(count / (double)pageSize);
+            AddRange(items);
+        }
+
+        public int PageIndex { get; set; }
+        public int TotalPage { get; set; }
+
+        public static PaginatedList<T> create(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
---
+++ MyWebApiApp/MyWebApiApp/Services/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using MyWebApiApp.Models;
+using System.Collections.Generic;
+
+namespace MyWebApiApp.Services
+{
+    public interface IProductRepository
+    {
+        List<ProductModel> getAll(string search, double? from, double? to, string sortBy, int page = 1);
+        //ProductVM createProduct(ProductModel product);
+    }
+}
